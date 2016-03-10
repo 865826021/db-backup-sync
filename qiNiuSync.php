c3a0083063d29168669fb0fe116ad8f28d7e5580
hiscaler <hiscaler@gmail.com>
2016-03-10 11:38:03 +0800

    重构日志信息处理

--- qiNiuSync.php
+++ qiNiuSync.php
@@ -21,8 +21,6 @@ if (isset($qiNiuConfig['accessKey']) && isset($qiNiuConfig['secretKey']) && isse
     $auth = new Auth($accessKey, $secretKey);
     $token = $auth->uploadToken($bucket);
 
-    $uploadMgr = new UploadManager();
-
     $dir = $config['fileDirectory'];
     $files = [];
     $handle = opendir($dir);
@@ -39,29 +37,40 @@ if (isset($qiNiuConfig['accessKey']) && isset($qiNiuConfig['secretKey']) && isse
     if ($files) {
         // 获取空间中的文件列表
         $bucketMgr = new BucketManager($auth);
-        list($iterms, $marker, $err) = $bucketMgr->listFiles($bucket, '', '', null);
+        list($iterms, $marker, $err) = $bucketMgr->listFiles($bucket, null, null, null);
         if ($err == null) {
             $uploadFiles = [];
             foreach ($iterms as $key => $item) {
                 $uploadFiles[$item['key']] = $item['hash'];
             }
-            $logFile = fopen(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR . date('Ymd') . '.log', "a") or die("Unable to open file!");
-            fwrite($logFile, "\r\n########## " . date('Y-m-d H:i:s') . " ##########\r\n");
-            foreach ($files as $file) {
+
+            $sign = ' ' . date('Y-m-d H:i:s') . ' ';
+            $logMessages = [str_pad($sign, 80, "#", STR_PAD_BOTH)];
+            $uploadMgr = new UploadManager();
+            $countFiles = count($files);
+            foreach ($files as $i => $file) {
                 echo "$file\r\n";
                 $key = basename($file);
                 // 判断文件是否已经上传
                 if (isset($uploadFiles[$key])) {
+                    $logMessages[] = str_pad($i + 1, $countFiles, '', STR_PAD_LEFT) . ': Ignore ' . $file;
                     continue;
                 }
                 list($ret, $err) = $uploadMgr->putFile($token, $key, $file);
                 if ($err !== null) {
                     // write log
-                    $txt = implode(' | ', $err) . "\r\n";
-                    fwrite($logFile, $txt);
+                    $txt = implode(' | ', $err);
+                    $logMessages[] = $txt;
+                } else {
+                    $logMessages[] = str_pad($i + 1, $countFiles, '', STR_PAD_LEFT) . ': Uploaded ' . $file;
                 }
             }
-            fwrite($logFile, "########## end ##########\r\n");
+            if (count($logMessages) == 1) {
+                $logMessages[] = 'Nothing';
+            }
+            $logMessages[] = str_pad($sign, 80, "#", STR_PAD_BOTH);
+            $logFile = fopen(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR . date('Ymd') . '.log', "a") or die("Unable to open file!");
+            fwrite($logFile, implode("\r\n", $logMessages) . "\r\n");
             fclose($logFile);
         }
     }
